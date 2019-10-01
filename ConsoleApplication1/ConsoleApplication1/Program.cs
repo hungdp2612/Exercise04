4f084ff40aa684baf56c286cefe4885ca007a4bc
hung2612 <hungdp2612@gmail.com>
2019-10-01 08:13:05 +0700

    ok

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,15 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("hello github");
+            Console.WriteLine(Square(4));
+            Console.WriteLine(Pow(2, 3));
+            Console.WriteLine(IsPrime(8));
+            Console.WriteLine(IsEven(3));
+            Console.WriteLine(Floor(4));
+            Console.WriteLine(Cube(-10));
+            Console.WriteLine(Ceil(4.6));
+            Console.WriteLine(Abs(6));
+
             Console.ReadKey();
         }
         public static bool IsOdd(int n)
@@ -20,6 +29,59 @@ namespace ConsoleApplication1
                 return true;
             }
             return false;
+        }
+        static int Abs(int n)
+        {
+            if (n < 0)
+            {
+                n = n * (-1);
+            }
+            return n;
+        }
+        static int Ceil(double x)
+        {
+            int n = (int)x;
+            if ((n - x) <= 0.5)
+                n = n + 1;
+            return n;
+
+            //int d = (int)(x + 0.5);
+            //return d;
+        }
+        static int Cube(int n)
+        {
+            return n * n * n;
+        }
+        static int Floor(float x)
+        {
+            return (int)x;
+        }
+        static bool IsEven(int n)
+        {
+            if (n % 2 == 0)
+                return true;
+            return false;
+        }
+        static bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+            for (int i = 2; i < n; i++) if (n % i == 0)
+                {
+                    return false;
+                }
+            return true;
+        }
+        static int Pow(int x, int y)
+        {
+            int rs = 1;
+            for (int i = 1; i <= y; i++)
+                rs = rs * x;
+            return rs;
+        }
+        static int Square(int n)
+        {
+            return n * n;
         }
 
     }
